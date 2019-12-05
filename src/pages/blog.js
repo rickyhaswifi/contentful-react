678feae1dadaf01703fa6d4928de157179982afe
Ricardo Bautista <rickyhaswifi@gmail.com>
2019-12-04 22:38:15 -0700

    React Contentful Starter

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -13,13 +13,13 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} >
-        <div style={{ background: '#fff' }}>
+        <div style={{ background: '#f2f2f2' }}>
           <Helmet title={siteTitle} />
           <div className={styles.hero}>
             Blog
           </div>
           <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
+            <h2 className="section-headline">RECENT articles!</h2>
             <ul className="article-list">
               {posts.map(({ node }) => {
                 return (
